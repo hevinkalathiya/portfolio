96c8b7bdf4b1b13a26864e2318ba8fd17d603447
Hevin <122536372+hevinkalathiya@users.noreply.github.com>
2024-02-28 15:12:11 +0530

    Add new work experience entry to Workexperience component

--- components/Workexperience.tsx
+++ components/Workexperience.tsx
@@ -4,6 +4,16 @@ import Heading from "./Heading";
 import MaxWidthWrapper from "./MaxWidthWrapper";
 const data = [
   {
+    id: 3,
+    companyLogo: "/images/upwork.png",
+    companyName: "Upwork",
+    position: "Fullstack Devloper",
+    startDate: "dec, 2023",
+    endDate: "Present",
+    description:
+      "Created company's in-house website using Next.js from scratch which has increased traffic by 200%. Developed Admin panel of a game using Node.js. Currently working on a React.js project.",
+  },
+  {
     id: 1,
     companyLogo: "/images/beit-logo.jpg",
     companyName: "BigElement IT Solutions",
@@ -23,23 +33,13 @@ const data = [
     description:
       "Created company's in-house website using Next.js from scratch which has increased traffic by 200%. Developed Admin panel of a game using Node.js. Currently working on a React.js project.",
   },
-  {
-    id: 3,
-    companyLogo: "/images/upwork.png",
-    companyName: "Upwork",
-    position: "Fullstack Devloper",
-    startDate: "dec, 2023",
-    endDate: "Present",
-    description:
-      "Created company's in-house website using Next.js from scratch which has increased traffic by 200%. Developed Admin panel of a game using Node.js. Currently working on a React.js project.",
-  },
 ];
 
 const Workexperience = () => {
   return (
     <MaxWidthWrapper className="mx-auto">
       <div className="py-10 px-2">
-        <Heading title={"Work Experience"} />
+        <Heading title={"Work Experience 🎢"} />
         <div className="grid lg:grid-cols-2 grid-cols-1 gap-x-12 gap-y-10">
           {data.map((item) => (
             <div
