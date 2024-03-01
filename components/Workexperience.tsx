9b4cb3820db7a277992da91bcd8d3d4d4fb5bfb7
Hevin <122536372+hevinkalathiya@users.noreply.github.com>
2024-03-01 14:34:46 +0530

    Refactor Workexperience component and add new images

--- components/Workexperience.tsx
+++ components/Workexperience.tsx
@@ -43,40 +43,36 @@ const Workexperience = () => {
         <div className="py-10 px-2">
           <Heading title={"Work Experience 🎢"} />
           <div className="grid lg:grid-cols-2 grid-cols-1 gap-x-12 gap-y-10">
-            {data.map((item) => (
+            {data.map((data) => (
               <div
-                key={item.id}
-                className="flex mt-5 items-start lg:gap-x-6 gap-x-4 max-w-2xl md:relative md:before:absolute before:bottom-0 before:top-[5rem] before:left-9 before:w-[1px] before:h-[calc(100%-70px)] dark:before:bg-zinc-800 before:bg-zinc-200"
+                key={data.id}
+                className="flex items-start lg:gap-x-6 gap-x-4 max-w-2xl relative before:absolute before:bottom-0 before:top-[5rem] before:left-9 before:w-[1px] before:h-[calc(100%-70px)] dark:before:bg-zinc-800 before:bg-zinc-200"
               >
                 <a
-                  href=""
+                  href={"data.url"}
                   rel="noreferrer noopener"
                   target="_blank"
-                  className="grid place-items-center dark:bg-primary-bg bg-secondary-bg border dark:border-zinc-800 border-zinc-200 min-h-[80px] min-w-[80px] p-2 rounded-md overflow-clip md:relative"
+                  className="grid place-items-center dark:bg-primary-bg bg-secondary-bg border dark:border-zinc-800 border-zinc-200 min-h-[80px] min-w-[80px] p-2 rounded-md overflow-clip relative"
                 >
                   <Image
-                    alt="Company Logo"
-                    loading="lazy"
-                    width="50"
-                    height="50"
-                    decoding="async"
-                    data-nimg="1"
-                    className="object-cover duration-300 "
-                    style={{ color: "transparent" }}
-                    src={item.companyLogo}
+                    src={data.companyLogo}
+                    className="object-cover duration-300"
+                    alt={`${data.companyName} logo`}
+                    width={50}
+                    height={50}
                   />
                 </a>
                 <div className="flex flex-col items-start">
-                  <h3 className="text-xl font-bold">{item.companyName}</h3>
-                  <p>{item.position}</p>
+                  <h3 className="text-xl font-bold">{data.companyName}</h3>
+                  <p>{data.position}</p>
                   <time className="text-sm text-zinc-500 mt-2 tracking-widest uppercase">
-                    {`${item.startDate} - `}
+                    {`${data.startDate} - `}
                     <span className="dark:text-primary-color text-tertiary-color">
-                      {item.endDate}
+                      {data.endDate}
                     </span>
                   </time>
                   <p className="tracking-tight dark:text-zinc-400 text-zinc-600 my-4">
-                    {item.description}
+                    {data.description}
                   </p>
                 </div>
               </div>
