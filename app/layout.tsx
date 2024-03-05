f0ae8b3ae131379d4760e44a3e1d6d2e1c9d2973
Hevin <122536372+hevinkalathiya@users.noreply.github.com>
2024-03-05 17:52:24 +0530

    add google meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,23 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        {/* Include the Google Analytics script */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-40XF7QS8M2"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-40XF7QS8M2');
+            `,
+          }}
+        ></script>
+      </head>
       <body
         className={cn(
           inter.className,
