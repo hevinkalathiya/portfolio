de39059167c01106df91aa30c235cb9c7feb91d3
Hevin <122536372+hevinkalathiya@users.noreply.github.com>
2024-03-06 18:25:24 +0530

    Update newsletter heading style

--- app/page.tsx
+++ app/page.tsx
@@ -61,7 +61,7 @@ export default function Home() {
       <Projects />
       <div className="h-[40rem] w-full rounded-md  relative flex flex-col items-center justify-center antialiased">
         <div className="max-w-2xl mx-auto p-4">
-          <h1 className="relative z-10 text-lg md:text-7xl bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600 text-center font-sans font-bold">
+          <h1 className="relative z-10 text-lg md:text-7xl bg-clip-text text-transparent bg-gradient-to-b from-neutral-600 to-neutral-800 dark:bg-gradient-to-b dark:from-neutral-200 dark:to-neutral-600 text-center font-sans font-bold">
             Subscribe to my newsletter
           </h1>
           <p></p>
