8dddfc2fc8c9c5e157b26cc00faed47fc43fc0cf
Hevin <122536372+hevinkalathiya@users.noreply.github.com>
2024-03-22 12:52:00 +0530

    Remove Projects component from Home page

--- app/page.tsx
+++ app/page.tsx
@@ -59,7 +59,6 @@ export default function Home() {
       <Hero />
       <Socials />
       <Workexperience />
-      <Projects />
       <div className="h-[40rem] w-full rounded-md  relative flex flex-col items-center justify-center antialiased">
         <div className="max-w-2xl mx-auto p-4">
           <h1 className="relative z-10 text-lg md:text-7xl bg-clip-text text-transparent bg-gradient-to-b from-neutral-600 to-neutral-800 dark:bg-gradient-to-b dark:from-neutral-200 dark:to-neutral-600 text-center font-sans font-bold">
