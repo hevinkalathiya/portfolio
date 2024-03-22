abf5b8551827252920516ac7da74b0ae427b530c
Hevin <122536372+hevinkalathiya@users.noreply.github.com>
2024-03-22 12:57:21 +0530

    Refactor newsletter subscription form layout

--- app/page.tsx
+++ app/page.tsx
@@ -54,50 +54,50 @@ export default function Home() {
   };
 
   return (
-    <TracingBeam className="pl:0 md:pl-10">
-    <div className="flex flex-col">
-      <Hero />
-      <Socials />
-      <Workexperience />
-      <div className="h-[40rem] w-full rounded-md  relative flex flex-col items-center justify-center antialiased">
-        <div className="max-w-2xl mx-auto p-4">
-          <h1 className="relative z-10 text-lg md:text-7xl bg-clip-text text-transparent bg-gradient-to-b from-neutral-600 to-neutral-800 dark:bg-gradient-to-b dark:from-neutral-200 dark:to-neutral-600 text-center font-sans font-bold">
-            Subscribe to my newsletter
-          </h1>
-          <p></p>
-          <p className="text-neutral-500 max-w-lg mx-auto my-2 text-sm text-center relative z-10">
-            Unlock the benefits of subscribing to our newsletter! Stay informed
-            with exclusive updates, special offers, and exciting news. Be the
-            first to know about our latest features and promotions. Join now and
-            experience the advantages of being part of our community.
-          </p>
-
-          <div className=" flex items-center justify-center gap-2">
-            <Input
-              type="email"
-              placeholder="hevinatwork@gmail.com"
-              className="rounded-lg border pr-2 border-neutral-800 focus:ring-2 focus:ring-teal-500  w-full relative z-50 h-10 "
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <Button
-              className="cursor-pointer z-50"
-              onClick={handleSubmit}
-              variant="secondary"
-              disabled={loading}
-            >
-              {loading ? "Subscribing..." : "Subscribe 🎇"}
-            </Button>
-          </div>
-          {errorMessage && (
-            <p className="text-red-500 text-sm mt-2 text-start z-50">
-              {errorMessage}
+    <TracingBeam className="">
+      <div className="flex flex-col">
+        <Hero />
+        <Socials />
+        <Workexperience />
+        <div className="h-[40rem] w-full rounded-md  relative flex flex-col items-center justify-center antialiased">
+          <div className="max-w-2xl mx-auto p-4">
+            <h1 className="relative z-10 text-lg md:text-7xl bg-clip-text text-transparent bg-gradient-to-b from-neutral-600 to-neutral-800 dark:bg-gradient-to-b dark:from-neutral-200 dark:to-neutral-600 text-center font-sans font-bold">
+              Subscribe to my newsletter
+            </h1>
+            <p></p>
+            <p className="text-neutral-500 max-w-lg mx-auto my-2 text-sm text-center relative z-10">
+              Unlock the benefits of subscribing to our newsletter! Stay informed
+              with exclusive updates, special offers, and exciting news. Be the
+              first to know about our latest features and promotions. Join now and
+              experience the advantages of being part of our community.
             </p>
-          )}
+
+            <div className=" flex items-center justify-center gap-2">
+              <Input
+                type="email"
+                placeholder="hevinatwork@gmail.com"
+                className="rounded-lg border pr-2 border-neutral-800 focus:ring-2 focus:ring-teal-500  w-full relative z-50 h-10 "
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <Button
+                className="cursor-pointer z-50"
+                onClick={handleSubmit}
+                variant="secondary"
+                disabled={loading}
+              >
+                {loading ? "Subscribing..." : "Subscribe 🎇"}
+              </Button>
+            </div>
+            {errorMessage && (
+              <p className="text-red-500 text-sm mt-2 text-start z-50">
+                {errorMessage}
+              </p>
+            )}
+          </div>
+          <BackgroundBeams className="bg-transparent" />
         </div>
-        <BackgroundBeams className="bg-transparent" />
       </div>
-    </div>
     </TracingBeam>
   );
 }
